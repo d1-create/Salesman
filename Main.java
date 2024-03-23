5134cffda075c44a430369b75ba7badd29141f7c
Dirushan <116972461+d1-create@users.noreply.github.com>
2024-03-23 14:27:50 +0000

    Optimise and add better comments to main.java

--- Main.java
+++ Main.java
@@ -8,8 +8,8 @@ import java.util.*;
 public class Main{
 
     //global variables
-    public static ArrayList<String> names_arr = new ArrayList<String>();
-    public static ArrayList<Integer> sales_arr = new ArrayList<Integer>();
+    public static ArrayList<String> names_arr = new ArrayList<String>(); //list of names updated from file
+    public static ArrayList<Integer> sales_arr = new ArrayList<Integer>();//list of sales updated from file
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         //startup file managing
@@ -17,17 +17,20 @@ public class Main{
         FileManaging.ReadToFile.ReadFileToStringArr("names.txt", names_arr); //Startup convert to array
         FileManaging.ReadToFile.ReadFileToIntegerArr("sales.txt", sales_arr); //Startup convert to array
     
-        int mode = SalesManaging.GetMenuMode(input);
+        int mode = SalesManaging.GetMenuMode(input);//show menu and get mode
 
+        //view salesman mode via the arrays
         if(mode==1){
             String name = BasicFunc.GetInput.getStringInput("Salesman Name: ", input);
             SalesManaging.ViewSalesman(names_arr, sales_arr, name);
         }
-        if(mode==2){
+        //Add salesman to file and list
+        else if(mode==2){
             String Salesname = BasicFunc.GetInput.getStringInput("Salesman Name:", input);
             int Sales = BasicFunc.GetInput.getIntInput("Money Made:", input);
             SalesManaging.AddSalesman(names_arr, sales_arr, Salesname, Sales);
         }
+        //Remove salesman to file and list
         if(mode==3){
 
         }
